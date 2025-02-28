43b4ec981f72162d588f82a5352fa1338a55ff0d
rishita das <er.rishitadas@gmail.com>
2025-03-01 00:19:46 +0530

    PracticeQUESTION

--- smallestElementsinarray.c
+++ smallestElementsinarray.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 int main(){
-int arr[]={25,54,89,65,75,34,91};
-int n= sizeof(arr)/sizeof(arr[0]);
+int arr[]={25,54,89,65,75,34,91};  //initialize array
+int n= sizeof(arr)/sizeof(arr[0]);   //initialize size
 int min=arr[0];
 for(int i=0; i<n; i++){
-    if(min>arr[i]){
+    if(min>arr[i]){     
        min=arr[i];
     }
 }
